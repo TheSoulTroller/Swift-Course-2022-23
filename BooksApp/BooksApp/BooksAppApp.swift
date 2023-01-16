75d4418a6156cbb603b76b1ff15659f18e8b850f
Alexandar Hristov <ahristov@astea.net>
2023-01-16 18:44:03 +0200

    BooksApp. Apple Developer Course.

---
+++ BooksApp/BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Alexandar Hristov on 1.12.22.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
